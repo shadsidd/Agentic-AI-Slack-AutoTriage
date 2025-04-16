6d621c73a0f14abf7f86e651758d7294f8f72e48
Shadab Siddiqui <shadsidd@users.noreply.github.com>
2025-04-16 10:26:22 +0530

    Update sample_data.py

--- sample_data.py
+++ sample_data.py
@@ -11,37 +11,37 @@ slack_messages = [
     {
         "text": """JR-Praetorian APP 7:33 PM
 Account: js_tilemedia(200304247802), Region: ap-south-1
-Instance: tm-08b748f2-2732-415a-9100d-25e3b5bc5512-live-cpu-0, (i-05e1678d505ebcb58), (13.235.57.48)
+Instance: tm-08248f2-2732-415a-9144230d-25e3b5bc5512-live-cpu-0, (i-05e4578d3435ebcb58), (13.235.57.48)
 SG: sg-0be620a6ee6f78157, [udp:2376,tcp:2112,udp:22,tcp:22,tcp:2376,udp:9100,udp:2112,tcp:9100]
 Owner:""",
         "timestamp": "2025-04-11T19:33:00Z"
     },
     {
         "text": """AWS-Moody APP 10:34 PM
-A command was executed inside a pod in the kube-system namespace on EKS Cluster hs-eks-video-prod-api-aps1. If this behavior is not expected, it may indicate that your credentials are compromised.
+A command was executed inside a pod in the kube-system namespace on EKS Cluster dh-eks-file-prod-api-aps1. If this behavior is not expected, it may indicate that your credentials are compromised.
 Severity: 5 MFA-USED: None
-Account Name: js_tilemedia(200304247802); Region: ap-south-1""",
+Account Name: js_tileamerica(2043504247802); Region: ap-south-1""",
         "timestamp": "2025-04-14T22:34:00Z"
     },
     {
         "text": """AWS-Moody APP 5:49 PM
 An unprotected port on EC2 instance i-0d379a348bfef22d75 is being probed.
 Severity: 2
-Account Name: js_tilemedia(200304247802); Region: ap-south-1""",
+Account Name: Europe_UK(200306547802); Region: ap-south-1""",
         "timestamp": "2025-04-14T17:49:00Z"
     },
     {
         "text": """AWS-Moody APP 6:32 PM
 Credentials for the EC2 instance role dp-dbr-ap-south-1-data-access were used from a remote AWS account 414351767826.
 Severity: 8 MFA-USED: None
-Account Name: js_tilemedia(200304247802); Region: ap-south-1""",
+Account Name: UC_CA(20033447802); Region: ap-south-1""",
         "timestamp": "2025-04-14T18:32:00Z"
     },
     {
         "text": """AWS-Moody APP 9:49 PM
-36.161.39.192.22 is performing SSH brute force attacks against i-0379a348bfef22d75.
+36.161.39.192.22 is performing SSH brute force attacks against i-0379a454fef22d75.
 Severity: 2
-Account Name: js_tilemedia(200304247802); Region: ap-south-1""",
+Account Name: IN_website(205404247802); Region: ap-south-1""",
         "timestamp": "2025-04-14T21:49:00Z"
     }
 ]
